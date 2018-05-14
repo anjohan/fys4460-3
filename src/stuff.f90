3339b3c0ffc1b9229c8f61bb847d8227b8ce5479
Anders Johansson <anjohan@uio.no>
2018-05-14 16:17:15 +0200

    small fixes

--- src/stuff.f90
+++ src/stuff.f90
@@ -20,4 +20,7 @@ program test
     real(kind=dp), dimension(:), allocatable :: bin_mids, n
 
     write(*,*) testfunc(3,[4,5,6])
+    do i = -5, 5
+        write(*,*), i, mod(i,4)
+    end do
 end program test
