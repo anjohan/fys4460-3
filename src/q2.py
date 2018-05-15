50afee773fa1df8ef247be028c2baa5744c63954
Anders Johansson <anjohan@uio.no>
2018-05-15 18:14:48 +0200

    delivered

--- src/q2.py
+++ src/q2.py
@@ -9,16 +9,17 @@ pc = 0.592746
 L = 2**9
 num_samples = 100
 
-num_ps = 50
-ps = np.zeros((2, num_ps))
-ps[0] = pc * (1 + 1e-9 + np.linspace(0, 0.1, num_ps))
-ps[1] = pc * (1 - 1e-9 - np.linspace(0, 0.1, num_ps))
+num_ps = 20
+ps = pc * (1 + np.logspace(np.log10(0.01), np.log10(0.15), num_ps))
 
-M_SC = np.zeros((2, num_ps))
+M_SC = np.zeros(num_ps)
 
-for i in range(2):
-    for j in tqdm.tqdm(range(num_ps)):
-        M_SC[i, j] = singlyconnected.M_SC(ps[i, j], L, num_samples)
+for j in tqdm.tqdm(range(num_ps)):
+    M_SC[j] = singlyconnected.M_SC(ps[j], L, num_samples)
 P_SC = M_SC / L**2
 
-np.savetxt("tmp/q2.dat", np.column_stack((*np.abs(ps - pc), *P_SC)))
+a, b = np.polyfit(np.log(np.abs(ps - pc)), np.log(P_SC), deg=1)
+P_SC_approx = np.exp(b) * (ps - pc)**a
+
+np.savetxt("tmp/q2.dat", np.column_stack((np.abs(ps - pc), P_SC, P_SC_approx)))
+np.savetxt("tmp/q2exp.dat", [a], fmt="%.3f")
