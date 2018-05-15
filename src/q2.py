9fb514adbec1ccdc4adda8694857d80851ce396e
Anders Johansson <anjohan@uio.no>
2018-05-15 16:07:16 +0200

    restructure figure

--- src/q2.py
+++ src/q2.py
@@ -9,12 +9,16 @@ pc = 0.592746
 L = 2**9
 num_samples = 100
 
-ps = pc * (np.linspace(0.9, 1.1, 20))
+num_ps = 50
+ps = np.zeros((2, num_ps))
+ps[0] = pc * (1 + 1e-9 + np.linspace(0, 0.1, num_ps))
+ps[1] = pc * (1 - 1e-9 - np.linspace(0, 0.1, num_ps))
 
-M_SC = np.zeros(len(ps))
+M_SC = np.zeros((2, num_ps))
 
-for i, p in enumerate(tqdm.tqdm(ps)):
-    M_SC[i] = singlyconnected.M_SC(p, L, num_samples)
+for i in range(2):
+    for j in tqdm.tqdm(range(num_ps)):
+        M_SC[i, j] = singlyconnected.M_SC(ps[i, j], L, num_samples)
 P_SC = M_SC / L**2
 
-np.savetxt("tmp/q2.dat", np.column_stack((ps - pc, P_SC)))
+np.savetxt("tmp/q2.dat", np.column_stack((*np.abs(ps - pc), *P_SC)))
