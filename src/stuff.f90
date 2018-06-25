8dd9dabbfd55ab82f9fcb9bc0b90ce6188d9a99c
Anders Johansson <anjohan@uio.no>
2018-06-25 14:54:56 +0200

    cleaning

--- src/stuff.f90
+++ src/stuff.f90
@@ -13,16 +13,9 @@ program test
     use percolation
     use stuffmod
     implicit none
-    character(len=:), allocatable :: string
-    integer, dimension(:,:), allocatable :: intarray
-    integer :: i,j, fileunit
-    real(kind=dp), dimension(:), allocatable :: bin_mids, n
 
     write(*,*) testfunc(3,[4,5,6])
     write(*,*) testfunc([1,2,3],4)
     write(*,*) testfunc([1,2,3],[4,5,6])
 
-    ! do i = -5, 5
-    !     write(*,*), i, mod(i,4)
-    ! end do
 end program test
